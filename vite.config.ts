d3949fcc072c8faefa158712f3daf006d51d3378
Shahroz Ahmed <shahroz.khan2124238@gmail.com>
2026-07-02 16:06:21 +0500

    Revert "feat: remove SSR flag, update wrangler configuration, and add cloudflare plugin for SSR support"
    This reverts commit 29e8577e2722b3e5795f2bc2b07a8439bd94b1f2.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
@@ -8,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [
-    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     tanstackStart({
       spa: { enabled: true },
